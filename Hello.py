be9ec06f9ae9e149cc54c6bac8ab732b86cccfc2
Naibei Val <naibeivalentine@gmail.com>
2019-11-18 20:36:03 +0300

    Python_Basics

---
+++ Hello.py
@@ -0,0 +1,69 @@
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+# Keywords - Reserved words in Python.
+# is                          def
+# and                         while
+# True
+# False
+# if
+# for
+# while
+# finally
+# try
+# or
+# class
+# Rules of Naming Variables/Functions/Classes
+# 1. A variable name can not contain spaces e.g my name
+# Error-> May Name
+# Instead,
+#         a)use one variable names
+#             e.g age,
+#         b)camelCase e.g goHome, myName
+#         c)Snake_type e.g go_home
+
+
+my_name = "Naibei"
+print(type(my_name))
+print(my_name)
+age = 16
+print(type(age))
+print(age)
+# gender = "F"
+married = False
+print(type(married))
+print(married)
+# my_name = "Val"
+# print(my_name)
+# print(age)
+# print(gender)
+
+# STRINGS
+
+z = "I love programming"
+# i=15 and 0=9
+# new_var = z[-1]
+# new_var = z[9:] gives characters to the end
+# new_var = z[9:15] the end is not included hence add 1
+new_var = z[9:16]
+#print(new_var)
+print(new_var)
+
+name = "Faith"
+contact = "071234567"
+new_var = contact[2:]
+print(new_var)
+
+# ID=21234
+# print(name)
+# print(contact)
+# print(ID)
+
+my_friend = "halima"
+print(my_friend.capitalize())
+x = my_friend.capitalize()
+print(x)
+y = x.lower()
+print(y)
+str.count(x)
